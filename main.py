521ec506050f541865f6a93400db5197dcfe1034
tyndev <17089219+tyndev@users.noreply.github.com>
2024-02-13 06:25:04 -0800

    create file tree

---
+++ main.py
@@ -0,0 +1,14 @@
+# FEATURES
+    # ADD BOOK - title, author, publication year
+    # REMOVE BOOK
+    # LIST ALL BOOKS - filter by author
+    # UPDATE BOOK
+    # SAVE AND LOAD LIBRARY DATA - SQLite to save data and load on start
+# CLASSES 
+    # BOOK - single level class
+    # LIBRARY - for features 
+
+   
+
+
+
